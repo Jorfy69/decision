bd6eb56edd450010d20d6a210e2e5c0d577d5b66
Jordan <84395275+Jorfy69@users.noreply.github.com>
2023-04-17 11:59:59 -0700

    Update choice.py

--- choice.py
+++ choice.py
@@ -11,8 +11,10 @@ def options(rand_highest_value):
         print(f'This choice wins: {choices[2]}')
     elif RAND ==4:
         print(f'This choice wins: {choices[3]}')
-    else:
+    elif RAND == 5:
         print(f'This choice wins: {choices[4]}') 
+    else:
+        print('To many or not enough chocies') 
     
 
 def main():
