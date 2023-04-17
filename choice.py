59259ab5b4dfdb06958463c8c2eb5eb4175f3425
Jordan <84395275+Jorfy69@users.noreply.github.com>
2023-04-17 11:58:47 -0700

    Increased the max amount of chocies

--- choice.py
+++ choice.py
@@ -1,16 +1,24 @@
 import random
+def options(rand_highest_value):
+    RAND = random.randint(1,int(rand_highest_value))
+    a = input('What choices would you like?(Max 5)\n[Please list them all out with one space pls]\nAwnser(s) = ')
+    choices = [a]
+    if RAND == 1:
+        print(f'This choice wins: {choices[0]}')
+    elif RAND ==2:
+        print(f'This choice wins: {choices[1]}')
+    elif RAND == 3:
+        print(f'This choice wins: {choices[2]}')
+    elif RAND ==4:
+        print(f'This choice wins: {choices[3]}')
+    else:
+        print(f'This choice wins: {choices[4]}') 
+    
 
 def main():
-    a = input('What is choice 1?')
-    b = input('What is choice 2?')
-    c = input('What is choice 3?')
-    RAND = random.randint(1, 3)
-    if RAND == 1:
-        print(f'Choice {a} wins!')
-    elif RAND == 2:
-        print(f'Choice {b} wins!')
-    else:
-        print(f'Choice {c} wins!')   
+   choice_selector = input("What number of options would you like? :: ")
+   options(choice_selector)
+        
 
 if __name__ == '__main__':
     main()
